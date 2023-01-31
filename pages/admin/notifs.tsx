b777b6935bf271a284bd70e659aec97156b14d19
Tom Cuillandre <tcuillandre@myges.fr>
2023-01-31 11:57:36 +0100

    Fix send notif form

--- pages/admin/notifs.tsx
+++ pages/admin/notifs.tsx
@@ -1,20 +1,10 @@
 import { RadioGroup } from "@headlessui/react";
 import React from "react";
-import { useForm } from "react-hook-form";
 import { LayoutAdmin } from "./layout"
 
 const AdminChatbot = () => {
-    const { register, handleSubmit } = useForm();
-    const [selected, setSelected] = React.useState('')
-
-    const onSubmit = (data: any) => {
-        console.log(data);
-    }
-
-    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        handleSubmit(onSubmit)()
-    }
+    const [selected, setSelected] = React.useState('alert-info')
+    const [message, setMessage] = React.useState('');
 
     const options = [
         { name: 'Information', value: 'alert-info' },
@@ -22,17 +12,19 @@ const AdminChatbot = () => {
         { name: 'Warning', value: 'alert-warning' },
     ]
 
-    const handleChange = (value: string) => {
-        setSelected(value)
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("selected", selected, "message", message)
+        // CALL API ICI 😉
     }
 
     return (
         <LayoutAdmin>
             <form onSubmit={handleFormSubmit} className='flex flex-col justify-between h-1/2'>
                 <div className="space-y-2 flex flex-col justify-center items-center">
-                    <input type='text' {...register('message', { required: true })} placeholder='Mon message de notification' className='w-full h-10 my-2 border border-gray-300 rounded-md p-2 focus:outline-none text-black' />
+                    <input type='text' required={true} value={message} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)} placeholder='Mon message de notification' className='w-full h-10 my-2 border border-gray-300 rounded-md p-2 focus:outline-none text-black' />
                     <RadioGroup className={'space-x-2 w-full flex justify-center'} defaultValue={selected}
-                        onChange={(value: string) => { handleChange; console.log(value); register('alertType', { value: value }) }}>
+                        onChange={(value: string) => setSelected(value)}>
                         {
                             options.map((option) => (
                                 <RadioGroup.Option
@@ -50,7 +42,7 @@ const AdminChatbot = () => {
                     </RadioGroup>
                 </div>
                 <div className="flex w-full justify-end">
-                    <button className="p-3 w-52 rounded-md shadow-lg bg-slate-100 hover:shadow-inner hover:shadow-black" >Envoyer la notification</button>
+                    <button className="p-3 w-52 rounded-md shadow-lg bg-slate-100 hover:shadow-inner hover:shadow-black">Envoyer la notification</button>
                 </div>
             </form>
         </LayoutAdmin >
